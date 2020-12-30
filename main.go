87ea9065f31a3dab78a5fa0ab83b4c0f905afb3a
charles williams <charlesclinton2003@gmail.com>
2020-12-30 12:35:32 +0100

    updated main

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/charleyvibez/lenslocked.com/views"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var homeView *views.View
+var contactView *views.View
+var faqView *views.View
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(faqView.Render(w, nil))
+}
+
+// A helper function that panics on any error
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	homeView = views.NewView("bootstrap","views/home.gohtml")
+	contactView = views.NewView("bootstrap","views/contact.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	http.ListenAndServe(":3000", r)
+}
